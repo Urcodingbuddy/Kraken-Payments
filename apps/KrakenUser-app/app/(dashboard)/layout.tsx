79fd2eb8b1e2d7c289dc10e05d4cc3feb9e52646
Urcodingbuddy <anshpethe110@gmail.com>
2024-10-18 01:11:34 +0530

    added packages/db

--- apps/KrakenUser-app/app/(dashboard)/layout.tsx
+++ apps/KrakenUser-app/app/(dashboard)/layout.tsx
@@ -8,8 +8,6 @@ export default function Layout({
     children: React.ReactNode;
 }): JSX.Element {
 
-    
-
     return (
         <div className="flex relative">
             <div className="border-r border-r-[#A704BF] min-h-full z-10">
