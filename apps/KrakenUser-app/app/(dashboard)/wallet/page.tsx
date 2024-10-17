453605e57a8936adda737f9952347febe1bd1781
Ansh Pethe <anshpethe110@gmail.com>
2024-10-18 02:58:43 +0530

    added mt-5 on wallet heading

--- apps/KrakenUser-app/app/(dashboard)/wallet/page.tsx
+++ apps/KrakenUser-app/app/(dashboard)/wallet/page.tsx
@@ -12,7 +12,7 @@ export default async function() {
   return <div className="h-[calc(100vh-4rem)] w-full bg-black flex-col justify-center  dark:bg-grid-white/[0.2] bg-grid-white/[0.2] relative flex">
     {/* Radial gradient for the container to give a faded look */}
     <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-    <h1 className="text-[#A704BF] font-extrabold text-4xl mb-2  md:mb-8  ml-20   md:ml-20 md:mt-16 z-10">Wallet</h1>
+    <h1 className="text-[#A704BF] font-extrabold text-4xl mb-2  md:mb-8  ml-20   md:ml-20 md:mt-16 mt-5 z-10">Wallet</h1>
         <div className="overflow-y-scroll h-full max-h grid grid-cols-1 md:grid-cols-2 justify-center gap-4 p-4 z-10">
             <div className="flex justify-end items-start">
                 <AddMoney />
