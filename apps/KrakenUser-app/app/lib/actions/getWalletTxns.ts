74457451b23e8ad352daf9104d04b4947b9cd657
Urcodingbuddy <anshpethe110@gmail.com>
2024-10-20 18:22:15 +0530

    Added sorting by Date

--- apps/KrakenUser-app/app/lib/actions/getWalletTxns.ts
+++ apps/KrakenUser-app/app/lib/actions/getWalletTxns.ts
@@ -6,9 +6,12 @@ import prisma from "@repo/db/client";
 export async function getWalletTxns() {
   const session = await getServerSession(authOptions);
   const txns = await prisma.walletTxn.findMany({
-      where: {
-          userId: session?.user?.id
-      }
+    where: {
+        userId: session?.user?.id,
+    },
+    orderBy: {
+        startTime: 'desc',
+    },
   });
   return txns.map((t: { startTime: any; amount: any; status: any; provider: any; }) => ({
       time: t.startTime,
