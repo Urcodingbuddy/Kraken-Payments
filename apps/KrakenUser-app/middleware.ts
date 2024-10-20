8ff5d3118af959b31f61c40052f86c9a9cd8da8b
Urcodingbuddy <anshpethe110@gmail.com>
2024-10-20 17:37:40 +0530

    updated middleWare for /home page ....

--- apps/KrakenUser-app/middleware.ts
+++ apps/KrakenUser-app/middleware.ts
@@ -1,31 +1,4 @@
-// import { withAuth } from "next-auth/middleware";
-// import { NextRequest, NextResponse } from "next/server";
 
-// let reqCount = 0;
-// export function middleware(request:NextRequest){
-//     reqCount++;
-//     console.log(`Request Count: ${reqCount}`);
-//     console.log(request.nextUrl.pathname);
-//     const sessionToken = request.cookies.get("next-auth.session-token");
-    
-//     if (request.nextUrl.pathname.startsWith("/(dashboard)")) {
-//         if (!sessionToken) {
-//           return NextResponse.redirect(new URL("/auth/signin", request.url));
-//         }
-//       }
-//     return NextResponse.next();  
-// }
-
-// export const config = {
-//     matcher: [
-//       "/(dashboard)/:path*",      // Protect /dashboard and all its child routes
-//       "/api/:path*",             // Protect all API routes
-//         "/"
-//     ],
-//   };
-
-
-// middleware.ts
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
 import type { NextRequest } from 'next/server';
@@ -40,6 +13,10 @@ export async function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl;
 
     // First, check if the user is logged in and trying to access the homepage ('/'), redirect them to '/home'
+    if (!token && pathname === '/') {
+        return NextResponse.next();
+    }
+
     if (token && pathname === '/') {
         return NextResponse.redirect(new URL('/home', req.url));
     }
